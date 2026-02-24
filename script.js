486e6e1a0c613f114945a26406252358df95a0a0
Korolkova Elizaveta <144662812+krlkva@users.noreply.github.com>
2026-02-24 12:54:09 +0300

    функции для перемещения клеток

--- script.js
+++ script.js
@@ -83,6 +83,137 @@ function updateBoardVisual() {
     }
 }
 
+function collectMovedTiles(direction) {
+    direction = direction.toLowerCase();
+    switch (direction) {
+        case 'up': return moveBoardUp();
+        case 'down': return moveBoardDown();
+        case 'left': return moveBoardLeft();
+        case 'right': return moveBoardRight();
+        default: return [];
+    }
+}
+
+function moveTileList(tiles) {
+    let movedTiles = [];
+    let gameBoardWidth = document.querySelector('.gameboard').offsetWidth;
+    let tileWidth = document.querySelector('.gameboard__tile').offsetWidth;
+    let ratio = (gameBoardWidth / tileWidth - 4) / 10;
+    
+    for (let k = 0; k < 4; k++) {
+        if (tiles[k] === 0) {
+            for (let l = k + 1; l < 4; l++) {
+                if (tiles[l] !== 0) {
+                    tiles[k] = tiles[l];
+                    tiles[l] = 0;
+                    movedTiles.push([l, k, (l - k + (ratio * 2 * (l - k)))]);
+                    break;
+                }
+            }
+        }
+
+        if (tiles[k] !== 0) {
+            for (let l = k + 1; l < 4; l++) {
+                if (tiles[l] === 0) continue;
+                if (tiles[l] === tiles[k]) {
+                    tiles[k] = tiles[l] * 2;
+                    tiles[l] = 0;
+                    movedTiles.push([l, k, (l - k + (ratio * 2 * (l - k)))]);
+                }
+                break;
+            }
+        }
+    }
+    
+    return movedTiles;
+}
+
+function moveBoardUp() {
+    let movedTiles = [];
+    for (let j = 0; j < 4; j++) {
+        let column = [];
+        for (let k = 0; k < 4; k++) column.push(gameBoard[k][j]);
+        let movedColumnTiles = moveTileList(column);
+        
+        for (let k = 0; k < movedColumnTiles.length; k++) {
+            movedTiles.push([
+                [movedColumnTiles[k][0], j],
+                [movedColumnTiles[k][1], j],
+                '0%, ' + (-movedColumnTiles[k][2]) * 100 + '%'
+            ]);
+        }
+    }
+    return movedTiles;
+}
+
+function moveBoardDown() {
+    let movedTiles = [];
+    for (let j = 0; j < 4; j++) {
+        let column = [];
+        for (let k = 3; k >= 0; k--) column.push(gameBoard[k][j]);
+        let movedColumnTiles = moveTileList(column);
+        
+        for (let k = 0; k < movedColumnTiles.length; k++) {
+            movedTiles.push([
+                [3 - movedColumnTiles[k][0], j],
+                [3 - movedColumnTiles[k][1], j],
+                '0%, ' + (movedColumnTiles[k][2]) * 100 + '%'
+            ]);
+        }
+    }
+    return movedTiles;
+}
+
+function moveBoardLeft() {
+    let movedTiles = [];
+    for (let i = 0; i < 4; i++) {
+        let row = [];
+        for (let k = 0; k < 4; k++) row.push(gameBoard[i][k]);
+        let movedRowTiles = moveTileList(row);
+        
+        for (let k = 0; k < movedRowTiles.length; k++) {
+            movedTiles.push([
+                [i, movedRowTiles[k][0]],
+                [i, movedRowTiles[k][1]],
+                (-movedRowTiles[k][2]) * 100 + '%, 0%'
+            ]);
+        }
+    }
+    return movedTiles;
+}
+
+function moveBoardRight() {
+    let movedTiles = [];
+    for (let i = 0; i < 4; i++) {
+        let row = [];
+        for (let k = 3; k >= 0; k--) row.push(gameBoard[i][k]);
+        let movedRowTiles = moveTileList(row);
+        
+        for (let k = 0; k < movedRowTiles.length; k++) {
+            movedTiles.push([
+                [i, 3 - movedRowTiles[k][0]],
+                [i, 3 - movedRowTiles[k][1]],
+                (movedRowTiles[k][2]) * 100 + '%, 0%'
+            ]);
+        }
+    }
+    return movedTiles;
+}
+
+function areMovesAvailable() {
+    // Проверяем, есть ли возможные ходы
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (gameBoard[i][j] === 0) continue;
+            
+            // Проверяем соседние клетки
+            if (j < 3 && gameBoard[i][j] === gameBoard[i][j + 1]) return true;
+            if (i < 3 && gameBoard[i][j] === gameBoard[i + 1][j]) return true;
+        }
+    }
+    return false;
+}
+
 function startNewGame() {
     gameBoard = [
         [0, 0, 0, 0],
